1f854168aeebe246600ce827752994b0ad7cded7
Mikhail Khvoinitsky <m_khvoinitsky@wargaming.net>
2020-02-23 13:36:07 +0300

    support --application

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,65 @@ extern crate libpulse_binding as pulse;
 
 use pulsectl::controllers::DeviceControl;
 use pulsectl::controllers::SinkController;
+use pulsectl::controllers::AppControl;
+
+trait VolumeControlTarget {
+    fn is_running(&self) -> bool;
+    fn name(&self) -> String;
+    fn description(&self) -> String;
+    fn is_default(&self, controller: &mut SinkController) -> bool;
+    fn is_muted(&self) -> bool;
+    fn mute(&self, mute: bool, controller: &mut SinkController);
+    fn volume(&self) -> pulse::volume::ChannelVolumes;
+    fn set_volume(&self, new_vol: pulse::volume::ChannelVolumes, controller: &mut SinkController);
+}
+
+impl VolumeControlTarget for pulsectl::controllers::types::DeviceInfo {
+    fn is_running(&self) -> bool {
+        self.state == pulsectl::controllers::types::DevState::Running
+    }
+    fn name(&self) -> std::string::String {
+        self.name.as_ref().unwrap().to_string()
+    }
+    fn is_default(&self, controller: &mut SinkController) -> bool {
+        self.name() == controller.get_server_info().unwrap().default_sink_name.unwrap()
+    }
+    fn is_muted(&self) -> bool { self.mute }
+    fn mute(&self, mute: bool, controller: &mut SinkController) {
+        let op = controller.handler.introspect.set_sink_mute_by_name(self.name.as_ref().unwrap(), mute, Option::Some(Box::new(|_| {})));
+        controller.handler.wait_for_operation(op).unwrap();
+    }
+    fn description(&self) -> std::string::String {
+        self.description.as_ref().unwrap().to_string()
+    }
+    fn volume(&self) -> pulse::volume::ChannelVolumes { self.volume }
+    fn set_volume(&self, new_vol: pulse::volume::ChannelVolumes, controller: &mut SinkController) {
+        controller.set_device_volume_by_name(&self.name(), &new_vol);
+    }
+}
+
+impl VolumeControlTarget for pulsectl::controllers::types::ApplicationInfo {
+    fn is_running(&self) -> bool { !self.corked }
+    fn name(&self) -> std::string::String {
+        self.name.as_ref().unwrap().to_string()
+    }
+    fn is_default(&self, _: &mut SinkController) -> bool { false }
+    fn is_muted(&self) -> bool { self.mute }
+    fn mute(&self, mute: bool, controller: &mut SinkController) {
+        controller.set_app_mute(self.index, mute).unwrap();
+    }
+    fn description(&self) -> std::string::String {
+        match self.proplist.get_str("application.name") {
+            Some(desc) => desc,
+            None => "Unspecified".to_string(),
+        }
+    }
+    fn volume(&self) -> pulse::volume::ChannelVolumes { self.volume }
+    fn set_volume(&self, new_vol: pulse::volume::ChannelVolumes, controller: &mut SinkController) {
+        let op = controller.handler.introspect.set_sink_input_volume(self.index, &new_vol, None);
+        controller.handler.wait_for_operation(op).unwrap();
+    }
+}
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = env!("CARGO_PKG_NAME"), about = "Very simple program to control volume of currently playing device", author = "Mikhail Khvoinitsky")]
@@ -15,6 +74,10 @@ struct Opt {
     /// Verbose mode
     #[structopt(short, long)]
     verbose: bool,
+
+    /// Control application, not device
+    #[structopt(short, long)]
+    application: bool,
 
     /// Increase volume of PERCENT
     #[structopt(long, value_name = "PERCENT", display_order(1))]
@@ -53,47 +116,31 @@ struct Opt {
     step_interval: u64,
 }
 
-fn main() {
-    let opt = Opt::from_args();
-
-    let mut tmp = std::env::temp_dir();
-    tmp.push(env!("CARGO_PKG_NAME"));
-    let tmpfile_base = tmp.to_str().unwrap();
-
-    let lockfile_path = format!("{}.{}", tmpfile_base, "lock");
-    let lockfile = std::fs::File::create(&lockfile_path).unwrap();
-    nix::fcntl::flock(std::os::unix::io::AsRawFd::as_raw_fd(&lockfile), nix::fcntl::FlockArg::LockExclusiveNonblock).expect("Already running");
-
-    let mut handler = SinkController::create();
-    let devices = handler
-        .list_devices()
-        .expect("Could not get list of playback devices");
-
+fn adjust_volume(targets: Vec<impl VolumeControlTarget>, opt: Opt, tmpfile_base: &str, mut controller: SinkController) {
     let prev_device_filename = format!("{}_{}", tmpfile_base, "previously_controlled_device_name");
     let (devices_to_control, write_prev_device) = {
-        let running_devices: Vec<_> = devices.iter().filter(|dev| dev.state == pulsectl::controllers::types::DevState::Running).collect();
+        let running_devices: Vec<_> = targets.iter().filter(|target| target.is_running()).collect();
         if running_devices.len() > 0 {
             println!("running_devices");
             (running_devices, true)
         } else {
             let prev_devices = match std::fs::read_to_string(&prev_device_filename) {
-                Ok(prev_device_name) => devices.iter().filter(|dev| prev_device_name == dev.name.as_ref().unwrap().as_str()).collect(),
-                Err(_) => std::vec::Vec::<&pulsectl::controllers::types::DeviceInfo>::new(),
+                Ok(prev_device_name) => targets.iter().filter(|target| prev_device_name == target.name()).collect(),
+                Err(_) => std::vec::Vec::<_>::new(),
             };
             if prev_devices.len() > 0 {
                 println!("prev_devices");
                 (prev_devices, false)
             } else {
                 println!("fallback devices");
-                let default_sink_name = handler.get_server_info().unwrap().default_sink_name.unwrap();
-                println!("default: {}", default_sink_name);
-                (devices.iter().filter(|dev| default_sink_name == dev.name.as_ref().unwrap().as_str()).collect(), false)
+                // Warning: here get_server_info() is called for every device which is not very optimal TODO: fix it somehow
+                (targets.iter().filter(|target| target.is_default(&mut controller)).collect(), false)
             }
         }
     };
 
     for device in devices_to_control {
-        let prev_notification_id_filename = format!("{}_{}_{}", tmpfile_base, device.name.as_ref().unwrap(), "notification-id");
+        let prev_notification_id_filename = format!("{}_{}_{}", tmpfile_base, device.name(), "notification-id");
         let mut prev_id: u32 = match std::fs::read_to_string(&prev_notification_id_filename) {
             Ok(prev_notification_id_str) => match prev_notification_id_str.trim().parse::<u32>() {
                 Ok(prev_notification_id) => prev_notification_id,
@@ -103,21 +150,21 @@ fn main() {
         };
 
         prev_id = if opt.mute_toggle {
-            let op = handler.handler.introspect.set_sink_mute_by_name(device.name.as_ref().unwrap(), !device.mute, Option::Some(Box::new(|_| {})));
-            handler.handler.wait_for_operation(op).unwrap();
+            let is_now_muted = !device.is_muted();
+            device.mute(is_now_muted, &mut controller);
 
             show_notification(
                 prev_id,
-                if !device.mute { pulse::volume::VOLUME_MUTED.0 } else { device.volume.avg().0 },
-                device.description.as_ref().unwrap(),
-                if !device.mute { opt.icon_muted.as_str() } else { opt.icon.as_str() },
+                if is_now_muted { pulse::volume::VOLUME_MUTED.0 } else { device.volume().avg().0 },
+                &device.description(),
+                if is_now_muted { opt.icon_muted.as_str() } else { opt.icon.as_str() },
                 opt.duration,
             )
         } else {
-            println!("is muted: {}", device.mute);
-            let mut muted = device.mute;
+            let mut muted = device.is_muted();
+            println!("is muted: {}", muted);
 
-            let old_volume = if muted && opt.increase.is_some() { pulse::volume::VOLUME_MUTED.0 } else { device.volume.avg().0 };
+            let old_volume = if muted && opt.increase.is_some() { pulse::volume::VOLUME_MUTED.0 } else { device.volume().avg().0 };
             let uimax = pulse::volume::Volume::ui_max().0;
 
             let factor: f64 = if opt.increase.is_some() {
@@ -163,24 +210,23 @@ fn main() {
             for (new_volume, sleep_at_the_end) in value_steps.into_iter().enumerate().map(|(i, step_value)| {
                 (step_value, i + 1 != value_steps_len)
             }) {
-                let mut new_vol_struct = device.volume.clone();
+                let mut new_vol_struct = device.volume().clone();
                 //new_vol_struct.set(new_vol_struct.len() as u32, pulse::volume::Volume(new_volume));
                 for vol in new_vol_struct.get_mut() {
                     vol.0 = new_volume;
                 }
 
-                println!("old: {}, new: {}, uimax: {}", device.volume.avg().0, new_vol_struct.avg().0, uimax);
-                handler.set_device_volume_by_name(device.name.as_ref().unwrap(), &new_vol_struct);
+                println!("old: {}, new: {}, uimax: {}", device.volume().avg().0, new_vol_struct.avg().0, uimax);
+                device.set_volume(new_vol_struct, &mut controller);
                 if muted && opt.increase.is_some() {
-                    let op = handler.handler.introspect.set_sink_mute_by_name(device.name.as_ref().unwrap(), false, Option::Some(Box::new(|_| {})));
-                    handler.handler.wait_for_operation(op).unwrap();
+                    device.mute(false, &mut controller);
                     muted = false;
                 }
 
                 prev_id = show_notification(
                     prev_id,
                     new_volume,
-                    device.description.as_ref().unwrap(),
+                    &device.description(),
                     opt.icon.as_str(),
                     opt.duration,
                 );
@@ -191,11 +237,31 @@ fn main() {
             }
 
             if write_prev_device {
-                std::fs::write(&prev_device_filename, device.name.as_ref().unwrap()).unwrap();
+                std::fs::write(&prev_device_filename, device.name()).unwrap();
             };
             prev_id
         };
         std::fs::write(&prev_notification_id_filename, prev_id.to_string()).unwrap();
+    }
+}
+
+fn main() {
+    let opt = Opt::from_args();
+
+    let mut tmp = std::env::temp_dir();
+    tmp.push(env!("CARGO_PKG_NAME"));
+    let tmpfile_base = tmp.to_str().unwrap();
+
+    let lockfile_path = format!("{}.{}", tmpfile_base, "lock");
+    let lockfile = std::fs::File::create(&lockfile_path).unwrap();
+    nix::fcntl::flock(std::os::unix::io::AsRawFd::as_raw_fd(&lockfile), nix::fcntl::FlockArg::LockExclusiveNonblock).expect("Already running");
+
+    let mut controller = SinkController::create();
+
+    if opt.application {
+        adjust_volume(controller.list_applications().expect("Could not get list of playback applications"), opt, tmpfile_base, controller);
+    } else {
+        adjust_volume(controller.list_devices().expect("Could not get list of playback devices"), opt, tmpfile_base, controller);
     }
 
     std::fs::remove_file(&lockfile_path).unwrap();
